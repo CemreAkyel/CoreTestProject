7c9a91c250ce873a52aab423712c9661d5ad6296
cemreakyel <cemreakyel@sakarya.edu.tr>
2018-10-06 16:56:30 +0300

    cmt

---
+++ CoreTestProject/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreTestProject.Controllers
+{
+    [Route("api/Test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "testghcvsdgchdsc", "csdcsjhbsdjhc" };
+        }
+    }
+}
